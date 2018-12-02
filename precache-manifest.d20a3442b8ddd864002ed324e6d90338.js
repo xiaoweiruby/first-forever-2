c6d5f43ebec90876f40f48099a5f600054024bc8
xiaowei <shenzhoudance@gmail.com>
2018-12-02 14:15:27 +0800

    initial commit

---
+++ precache-manifest.d20a3442b8ddd864002ed324e6d90338.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fead4b61aceb5ea59c3",
+    "url": "/static/css/main.a4215d24.chunk.css"
+  },
+  {
+    "revision": "1fead4b61aceb5ea59c3",
+    "url": "/static/js/main.1fead4b6.chunk.js"
+  },
+  {
+    "revision": "61d2d465a34f43a2be01",
+    "url": "/static/js/1.61d2d465.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "298dec472ebcfc9132b1618644da6cc2",
+    "url": "/static/media/logo.298dec47.svg"
+  },
+  {
+    "revision": "161ca59cce6f11646e8b79bc341af2bc",
+    "url": "/index.html"
+  }
+];
